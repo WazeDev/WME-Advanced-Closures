032746dcb70f85cd46a5a151a4790e301546872c
dummyd2 <dummyaydf@yahoo.fr>
2016-09-21 21:02:18 +0200

    35: mteID can have negative numbers (CSV)

--- src/include/csv.js
+++ src/include/csv.js
@@ -112,7 +112,7 @@ WMEAC.csv.push(new WMEAC.ClassCSV({version: 1, regexpValidation: [/.*/, // 1st c
                                                                   /^(\d+(;|$))+/, // seg ID list
                                                                   /(lon=(-?\d+\.?\d*)&lat=(-?\d+\.?\d*))|(lat=(-?\d+\.?\d*)&lon=(-?\d+\.?\d*))/, // lonlat
                                                                   /^\d$/, // zoom
-                                                                  /(^$)|(^\d+\.\d+\.\d+$)/ // MTE ID is empty or digits.digits.digits
+                                                                  /(^$)|(^-?\d+\.-?\d+\.-?\d+$)/ // MTE ID is empty or digits.digits.digits
                                                                   ]}));
                                                                   
 WMEAC.buildInlineClosureUI = function (closure, action)
