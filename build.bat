0c40cf6c2db58afc0131c432631b0af5638dd1b4
justins83 <justins83.waze@gmail.com>
2018-07-05 15:11:29 -0400

    New build
    Updating the version for the new build.
    
    Updateing the build.bat to use version 17 of EasyPHP server

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 move ac-v*.js archives
-set phppath="C:\Program Files (x86)\EasyPHP-DevServer-14.1VC11\binaries\php\php_runningversion\php.exe"
+set phppath="C:\Program Files (x86)\EasyPHP-DevServer-17\eds-binaries\php\php713vc14x86x180705145909\php.exe"
 
 %phppath% build.php
 
