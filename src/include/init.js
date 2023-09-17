ff563719a452a7539fe9b4eb27c020d79b84271c
Dave Alverson <daveacincy.waze@gmail.com>
2023-09-17 14:38:29 -0400

    use new API to wait for wme ready

--- src/include/init.js
+++ src/include/init.js
@@ -1,78 +1,22 @@
 WMEAC.bootstrapAC = function ()
 {
-    window.setTimeout(WMEAC.initialize, 500);
+    if (W?.userscripts?.state?.isReady) {
+        WMEAC.initialize();
+    } else {
+        document.addEventListener("wme-ready", WMEAC.initialize, {
+            once: true,
+        });
+    }
 };
 
 
 WMEAC.initialize = function ()
 {
     WMEAC.log ("init");
-    WMEAC.waitForWaze(function () {
-        WMEAC.load();
-        WMEAC.log("presets", WMEAC.presets);
-        WMEAC.initUI();
-    });
+    WMEAC.load();
+    WMEAC.log("presets", WMEAC.presets);
+    WMEAC.initUI();
     WMEAC.log ("init done");
 };
 
-
-WMEAC.waitForWaze = function(handler)
-{
-    var APIRequired=[{o: "W"},
-                     {o: "W.model"},
-                     {o: "W.map"},
-                     {o: "W.loginManager"},
-                     {o: "W.app.layout.model"},
-                     {o: "W.Config"},
-                     {o: "W.controller"}
-                    ];
-    for (var i=0; i<APIRequired.length; i++)
-    {
-        var path=APIRequired[i].o.split('.');
-        var object=window;
-        for (var j=0; j<path.length; j++)
-        {
-            object=object[path[j]];
-            if (typeof object == "undefined" || object == null)
-            {
-                window.setTimeout(function () { WMEAC.waitForWaze(handler); }, 500);
-                return;
-            }
-        }
-    }
-    
-    
-    var userInfo = WMEAC.getId('user-info');
-    if (userInfo==null)
-    {
-        window.setTimeout(function () { WMEAC.waitForWaze(handler); }, 500);
-        return;
-    }
-
-    var navTabs=userInfo.getElementsByTagName('ul');
-    if (navTabs.length==0)
-    {
-        window.setTimeout(function () { WMEAC.waitForWaze(handler); }, 500);
-        return;
-    }
-    if (typeof(navTabs[0])=='undefined')
-    {
-        window.setTimeout(function () { WMEAC.waitForWaze(handler); }, 500);
-        return;
-    }
-
-    var tabContents=userInfo.getElementsByTagName('div');
-    if (tabContents.length==0)
-    {
-        window.setTimeout(function () { WMEAC.waitForWaze(handler); }, 500);
-        return;
-    }
-    if (typeof(tabContents[0])=='undefined')
-    {
-        window.setTimeout(function () { WMEAC.waitForWaze(handler); }, 500);
-        return;
-    }
-
-    handler();
-};
 
